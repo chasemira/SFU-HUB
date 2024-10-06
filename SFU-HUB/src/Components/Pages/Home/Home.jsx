1d08a8b2680d2aecbe220cd93c3d64653cf3bc12
Jasleen Kaur <jka349@sfu.ca>
2024-10-05 21:07:23 -0700

    commiting calendar

--- SFU-HUB/src/Components/Pages/Home/Home.jsx
+++ SFU-HUB/src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import Calendar from "./calendar";
+import './Home.css'; 
 
 function Home() {
   return (
-    <div className='home'>
-        <h1>Home</h1>
+    <div>
+      {/*home section*/}
+      <section className='hero'>
+        <h1>welcome to sfu hub</h1>
+        <h1>testing </h1>
+        <h1>this is where the hero section will be later</h1>
+        <p>ur one stop hub for all things sfu</p>
+      </section>
+
+      
+      {/*calendar section*/}
+      <section className='calendar-section'>
+        <Calendar />
+      </section>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
