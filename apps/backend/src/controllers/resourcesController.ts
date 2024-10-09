bafcb1c1cd0c024946372a4786354ba1112497db
Evan Feng <efa34@sfu.ca>
2024-10-09 10:29:42 -0700

    fix: readd backend

---
+++ apps/backend/src/controllers/resourcesController.ts
@@ -0,0 +1,17 @@
+import type { Response } from 'express';
+import type { OperationsRequest } from '../@types/helpers';
+import resourcesService from '../services/resourcesService.js';
+
+const getResources = async (
+    _req: OperationsRequest<'getResources'>,
+    res: Response,
+): Promise<void> => {
+    // Calling the service
+    const resources = await resourcesService.getEvents();
+
+    res.status(200).json(resources);
+};
+
+export default {
+    getResources: getResources,
+};
