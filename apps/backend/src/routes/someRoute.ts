cd8ec4cdda312ae076dc8062ac7666d4bd2f6507
Kandisa Agarwal <kaa80@sfu.ca>
2024-10-09 00:12:42 -0700

    done

--- apps/backend/src/routes/someRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import someController from '../controllers/someController.js';
-
-const router = express.Router();
-
-router.get('/:someId', someController.getSome);
-
-export default router;
