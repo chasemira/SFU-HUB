b31ddbbd74a66cd672a279757ff02f0eab3161bb
Kandisa Agarwal <kaa80@sfu.ca>
2024-10-29 19:09:29 -0700

    commit try

--- apps/frontend/src/Components/Pages/Transportation/Transportation.jsx
+++ apps/frontend/src/Components/Pages/Transportation/Transportation.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function Transportation() {
   return (
     <div className='transportation'>
-        <h1>Transportation</h1>
+      <h1 className='trans_header'>Transportation</h1>
+      <div className='trans_content'>
+        <h2>Select SFU Campus</h2>
+        <div className='campus_dropdown'></div>
+      </div>
     </div>
   )
 }
